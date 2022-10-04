a49685f76905fd4b1c47990e6ca94da3797a8b2f
Sameer-Agrawal <sameerag0812@outlook.com>
2022-10-05 00:02:56 +0530

    Closure in action, another variety of function invocation

--- Conceptual Assignment/assignment2/test-2/Q3.js
+++ Conceptual Assignment/assignment2/test-2/Q3.js
@@ -3,18 +3,20 @@
 // f(x, y)
 // f(x)(y)
 
-//solution
-function f(x,y){
-    if(y == undefined){
-       return function(y){
-           return y*x;
-       } 
-    }else{
-        return y*x;
+// Task --> Function, returning product, of provided argument's, with variety of function invocation 
+function f(x, y){
+    if(y == undefined){  // Function invocation, single variable is provided as an argument
+        // Return a function, inturn invoked with second argument
+        function internal(y){  // Internal function, maintain lexical scope, that is reference to x 
+            return x*y;
+        }
     }
+    return x*y;
 }
 
+console.log(f(5)(4)); // Function returned, with an invocation is further invoked with 4
+// Returned, function form an closure with lexical scope, and maintain reference to x
+// Function invocation, which inturn return a function, maintain it's lexical scope
 console.log(f(5,4));
-console.log(f(5)(4));
 
 
