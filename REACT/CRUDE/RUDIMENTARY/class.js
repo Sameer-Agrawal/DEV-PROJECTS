5f583c307670cee2b53ba66d7bf16f2ae47b24ef
Sameer-Agrawal <sameerag0812@outlook.com>
2022-10-21 01:48:18 +0530

    React state, counter

--- REACT/CRUDE/RUDIMENTARY/class.js
+++ REACT/CRUDE/RUDIMENTARY/class.js
@@ -5,11 +5,31 @@
 class Counter extends React.Component{  // React.Component, represent a function component
     constructor(){
         super();  // The super keyword is used to call the constructor of its parent class to access the parent's properties and methods
+        this.state = {  // Represent state object, counter component
+            count : 0
+        }
     }
+
+    // The state is an instance (subject to mutation's) of React class component, which inturn, can be defined as an object of a set of observable properties that influence the behavior of the component. In other words, the state of a component is an object that holds some information that may change over the lifetime of the component.
+
+    incrementHandler = () => {  // Represent, increment handler
+        // To change a value in the state object, use the this.setState() method.
+        // When a value in the state object changes, the component will re-render, meaning that the UI output will change according to the new value.
+        this.setState({ count : this.state.count + 1 });  // Expect, an object as an argument
+    }
+
+    decrementHandler = () => {
+        this.setState({ count : this.state.count - 1 });
+    }
+
     render(){  // The render() method is invoked, with a mutation in react component. Component state mutation bring about, component UI, regeneration
         return(  // Expected, syntax
             // A common pattern in React is for a component to return multiple component. Fragments let you group a list of children without adding extra nodes to the DOM
-            <h1>Hey there, nice to meet you</h1>
+            <React.Fragment>
+                <button onClick={this.incrementHandler}>Increment</button>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.decrementHandler}>Decrement</button>
+            </React.Fragment>
         );
     }
 }
