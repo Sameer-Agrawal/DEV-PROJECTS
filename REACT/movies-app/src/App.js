e8b64be3e00b87811f067a7d550c7e298f0ff3cc
Sameer-Agrawal <sameerag0812@outlook.com>
2022-10-22 18:13:56 +0530

    Catalogue structure + abstract UI

--- REACT/movies-app/src/App.js
+++ REACT/movies-app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import NavBar from "./component's/NavBar";
 import Banner from "./component's/Banner";
+import Catalogue from "./component's/Catalogue";
 import './App.css'
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar/>
-      <Banner/>
+      <div className="parentElement">
+        <NavBar/>
+        <Banner/>
+        <Catalogue/>
+      </div>
     </React.Fragment>
   );
 }
---
+++ REACT/movies-app/src/component's/Catalogue.jsx
@@ -0,0 +1,153 @@
+// Class component
+import { Component } from 'react';
+import React from 'react';
+import '../UI/Catalogue.css';
+
+class Catalogue extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return(  // JSX, expected
+        <React.Fragment>
+            <div className="catalogueElement">
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+                <div className="cardElement">
+                    <img src="https://th.bing.com/th/id/OIP.bgZRya9d6c9saeEppWC1AQHaEK?pid=ImgDet&rs=1" className="cardImageElement" alt="Movie representation"/>
+                    <div className="cardMetadataClosure">
+                        <h5 className="cardTitleElement">Title here!</h5>
+                        <div className="appendContainer"><button className="appendElement">Append to preferable</button></div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+        )
+    }
+}
+
+export default Catalogue;
