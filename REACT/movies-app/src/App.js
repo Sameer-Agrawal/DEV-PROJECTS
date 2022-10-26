2841958521112ec831e6ddd472e97a0c755877e1
Sameer-Agrawal <sameerag0812@outlook.com>
2022-10-26 10:17:17 +0530

    Preference component UI enhancement, routing

--- REACT/movies-app/src/App.js
+++ REACT/movies-app/src/App.js
@@ -3,16 +3,25 @@ import NavBar from "./component's/NavBar";
 import Banner from "./component's/Banner";
 import Catalogue from "./component's/Catalogue";
 import Preference from "./component's/Preference";
-import './App.css'
+import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
       <div className="parentElement">
-        <NavBar/>
-        {/* <Banner/>
-        <Catalogue/> */}
-        <Preference/>
+
+        <BrowserRouter>
+          <NavBar/>
+          {/* Routes hold's up route */}
+          {/* In previous versions of React Router you had to order your routes a certain way to get the right one to render when multiple routes matched an ambiguous URL. V6 is a lot smarter and will pick the most specific match so you don't have to worry about that anymore. */}
+          <Routes>
+            {/* element attribute specify, component to render, provided specific path */}
+            <Route path="/" element = { <React.Fragment> <Banner/> <Catalogue/> </React.Fragment> } ></Route>
+            <Route path="/preference" element = { <Preference/> }></Route>
+          </Routes>
+        </BrowserRouter>
+
       </div>
     </React.Fragment>
   );
--- REACT/movies-app/src/component's/Preference.jsx
+++ REACT/movies-app/src/component's/Preference.jsx
@@ -7,20 +7,61 @@ import { movies } from '../Metadata/data.js'
 class Preference extends Component {
     constructor() {
         super();
+        this.state = { category : ["Absolute category"] }
     }
+
+    // categoryMaintainance() {  // Category maintainance, provided metadata
+    //     const genreIdentifier = { 28 : "Action" , 12 : "Adventure" , 16 : "Animation" , 35 : "Comedy" , 80 : "Crime" , 99 : "Documentary" , 18 : "Drama" , 10751 : "Family" , 14 : "Fantasy" , 36 : "History" , 27 : "Horror" , 10402 : "Music" , 9648 : "Mystery" , 10749 : "Romance" , 878 : "Science Fiction" , 10770 : "TV Movie" , 53 : "Thriller" , 10752 : "War" , 37 : "Western" }  // Represent genre, provided genre identifier
+
+    //     const categoryArray = this.state.category;  // Represent, metadata category
+
+    //     this.state.metadata.map( (dataElement) => {  // Looping through, metadata
+
+    //         const genre = genreIdentifier[dataElement.genre_ids[0]];
+
+    //         if(!(categoryArray.includes(genre))){  // The includes() method returns true if an array contains a specified value.
+    //             categoryArray.push(genre);  // Category maintainance
+    //         }  
+        
+    //     });
+
+
+    //     this.setState( { category : [...categoryArray] } );  // Maintainance, component state
+    // }
+
     render() {
         const metadata = movies.results;  // Represent, metadata
-        const genreIdentifier = { 28 : "Action" , 12 : "Adventure" , 16 : "Animation" , 35 : "Comedy" , 80 : "Crime" , 99 : "Documentary" }  // Represent genre, provided genre identifier
+        const genreIdentifier = { 28 : "Action" , 12 : "Adventure" , 16 : "Animation" , 35 : "Comedy" , 80 : "Crime" , 99 : "Documentary" , 18 : "Drama" , 10751 : "Family" , 14 : "Fantasy" , 36 : "History" , 27 : "Horror" , 10402 : "Music" , 9648 : "Mystery" , 10749 : "Romance" , 878 : "Science Fiction" , 10770 : "TV Movie" , 53 : "Thriller" , 10752 : "War" , 37 : "Western" }  // Represent genre, provided genre identifier
+
+        const categoryArray = this.state.category;  // Represent, metadata category
+
+        metadata.map( (dataElement) => {  // Looping through, metadata
+
+            const genre = genreIdentifier[dataElement.genre_ids[0]];
+
+            if(!(this.state.category.includes(genre))){  // The includes() method returns true if an array contains a specified value.
+                categoryArray.push(genre);  // Category maintainance
+            }  
+        
+        });
+
+        this.setState( { category : [...categoryArray] } );  // Maintainance, component state
+
         return(  // JSX, expected
             <React.Fragment>
                 <div className="preferenceElement">
 
                     <div className="col-3 filterElement">
                         <ul className="list-group">
-                            <li className="row list-group-item">Favourites</li>
-                            <li className="row list-group-item">Action</li>
-                            <li className="row list-group-item">Comedy</li>
-                            <li className="row list-group-item">Adventure</li>
+
+                            {
+                                categoryArray.map( (category) => {  // Looping through, category
+                                    return(
+                                        <li className="row list-group-item">{ category }</li>
+                                    );
+                                } )
+                            }
+                            
                         </ul>
                     </div>
 
@@ -47,6 +88,7 @@ class Preference extends Component {
                             <tbody>
                                 {
                                     metadata.map( (dataElement) => {  // Looping through, metadata
+
                                         return(  // Expected, row
                                             <tr>
                                                 <td className="titleClosure"><img src={`https://image.tmdb.org/t/p/original${dataElement.backdrop_path}`} alt="Image representation" class="dataElementImage"/><h6 className="dataElementTitle">{dataElement.title}</h6></td>
@@ -56,8 +98,11 @@ class Preference extends Component {
                                                 <td><button type="button" className="btn btn-danger">Danger</button></td>
                                             </tr>
                                         );
+
                                     } )
-                                }        
+
+                                }     
+
                             </tbody>
                         </table>
                     </div>
