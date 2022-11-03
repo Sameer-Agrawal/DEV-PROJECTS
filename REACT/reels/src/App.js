cdbd0fb78b75711dc86d2752ce1acd91d54ee984
Sameer-Agrawal <sameerag0812@outlook.com>
2022-11-03 16:23:30 +0530

    Abstract, firebase setup

---
+++ REACT/reels/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+// Firebase is a set of hosting services for any type of application. It offers NoSQL and real-time hosting of databases, content, social authentication, and notifications, or services, such as a real-time communication server
+// SDK --> SDK stands for Software Development Kit. It is a collection of software development tools, which can be used to create and develop applications for a specific device or operating system.
---
+++ REACT/reels/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions / functionality you need, from the SDK's you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {  // Web application, configuration
+  apiKey: "AIzaSyDxqVtM2gYv7NWv68NRE8t96tBZKqmKt-g",
+  authDomain: "instagram-reel-s.firebaseapp.com",
+  projectId: "instagram-reel-s",
+  storageBucket: "instagram-reel-s.appspot.com",
+  messagingSenderId: "1072420793",
+  appId: "1:1072420793:web:cd2580032e8b065938c868"
+};
+
+// Initialize Firebase
+const application = initializeApp(firebaseConfig);
