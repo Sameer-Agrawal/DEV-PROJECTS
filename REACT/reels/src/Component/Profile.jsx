07201ec8f86d88174264823a68d18db4e5130e4f
Sameer-Agrawal <sameerag0812@outlook.com>
2022-12-11 16:55:07 +0530

    Reels, UI enhancement

--- REACT/reels/src/Component/Profile.jsx
+++ REACT/reels/src/Component/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Process from './Process'
 import Navigation from './Navigation.jsx'
 import { context } from '../App';
 import { useContext , useState , useEffect } from 'react';
@@ -13,7 +14,7 @@ function Profile() {
     const [ datum , mutateDatum ] = useState(null);  // Definition, datum state
     const [ blunder , mutateBlunder ] = useState(null);  // Definition, blunder state
 
-    useEffect( () => {  // Callback invocation, provided component mount
+    useEffect( () => {  // Callback invocation, provided component mount together with mutation, active customer datum state
         (async function() {  // Immediately invoked, functional expression
             // console.log("Here, inside asynchronous callback")
             
@@ -25,41 +26,43 @@ function Profile() {
                     // Blunder, handling
                     // console.log('Document datum, ' , datum.data());  // Represent, document object                        
                     const object = datum.data()
-
+                    console.log(object)
                     await mutateDatum(object);  // Datum state, maintainance
                 }
             }
 
         })();
-    } , [] )
+    } , [ object ] )
 
 
     return (
         <React.Fragment>
-                <div className="profileOutmostContainer">
-                    {/* <Navigation/> */}
-                    <div className="metadataContainer">
-                        <div className="portraitContainer">
-                            <img src="https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg" alt="Visual representation" />
-                        </div>
-                        <div className="rightMetadataContainer">
-                            <div className="topRowMetadata">
-                                <div className="identifierElement"> <h1>beingsam_08</h1> </div>
-                                <div className="mutateMetadata"> <button>mutate</button> </div>
+                { datum == null ? <Process/> : 
+                    <div className="profileOutmostContainer">
+                        <div className="profileNavigationContainer"><Navigation/></div>
+                        <div className="metadataContainer">
+                            <div className="portraitContainer">
+                                <img src="https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg" alt="Visual representation" />
                             </div>
-                            <div className="numericMetadata">
-                                <div className="assetContainer childrenElement"> <h1>15 asset</h1> </div>
-                                <div className="admirerContainer childrenElement"> <h1>465 admirer</h1> </div>
-                                <div className="iconContainer childrenElement"> <h1>518 idol</h1> </div>
-                            </div>
-                            <div className="baseRowMetadata">
-                                <div className="denominationElement childrenElement"> <h1>Sameer Agrawal</h1> </div>
-                                <div className="biographyElement childrenElement"> <h1>I SEE EVERYTHING</h1> </div>
-                                {/* <div className="accessoryElement"></div> */}
+                            <div className="rightMetadataContainer">
+                                <div className="topRowMetadata">
+                                    <div className="profileIdentifierElement"> <h1>{ datum.alphabetic_identifier }</h1> </div>
+                                    <div className="mutateMetadata"> <button>mutate</button> </div>
+                                </div>
+                                <div className="numericMetadata">
+                                    <div className="assetContainer childrenElement"> <h1>15 asset</h1> </div>
+                                    <div className="admirerContainer childrenElement"> <h1>465 admirer</h1> </div>
+                                    <div className="iconContainer childrenElement"> <h1>518 idol</h1> </div>
+                                </div>
+                                <div className="baseRowMetadata">
+                                    <div className="denominationElement childrenElement"> <h1>{ datum.denomination }</h1> </div>
+                                    <div className="biographyElement childrenElement"> <h1>I SEE EVERYTHING</h1> </div>
+                                    {/* <div className="accessoryElement"></div> */}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                }
         </React.Fragment>
     );
 }
