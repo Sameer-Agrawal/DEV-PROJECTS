03d75913ea77d9cdd71e6dbb4217529710badc41
Sameer-Agrawal <sameerag0812@outlook.com>
2023-01-16 14:11:07 +0530

    HTML response, provided specific request

---
+++ BACK/Rudimentary/Fabrication.js
@@ -0,0 +1,28 @@
+const http = require('http');  // Import HTTP module 
+const fs = require('fs');  // JavaScript file-system module help store, access, and manage data on operating system.
+
+// Callback invocation provided request
+const server = http.createServer( ( request , response ) => { 
+
+    // Content-Type --> 'text/plain'
+    // response.setHeader( 'Content-Type', 'text/plain' );
+    // response.write( ` Response corresponding request, client URL --> ${ request.url }` );
+
+    // Content-Type --> 'text/html'
+    response.setHeader( 'Content-Type', 'text/html' );
+
+    if( request.url == '/' ){  // Represent, HOME snippet request
+        fs.readFile( './View/home.html' , ( blunder , datum ) => {
+            if( blunder ){ console.log( 'Blunder' ) }
+            else{ response.end( datum ) }
+        } );  // Read document provided non-blocking call stack, asynchronous manner
+    }else if( request.url == '/about' ){  // Represent ABOUT snippet request
+        fs.readFile( './View/about.html' , ( blunder , datum ) => {
+            if( blunder ){ console.log( 'Blunder' ) }
+            else{ response.end( datum ) }
+        } );
+    }
+
+} );  // createServer() method turn system into an HTTP server
+
+server.listen( 3000 , 'localhost' , () => { console.log('Server streaming, port 3000') } )
