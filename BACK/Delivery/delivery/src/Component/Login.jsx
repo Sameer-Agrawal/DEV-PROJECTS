8bd6bc86f25212a4a580ea7f51538ed1bf75f99c
Sameer-Agrawal <sameerag0812@outlook.com>
2023-01-19 23:40:51 +0530

    Login component, UI enhancement together with, Abstract state maintainance

--- BACK/Delivery/delivery/src/Component/Login.jsx
+++ BACK/Delivery/delivery/src/Component/Login.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 import '../UI/Login.css'
 
 function Login() {
+
+    const [ identifier , mutateIdentifier ] = useState('');  // State definition
+    const [ credential , mutateCredential ] = useState('');
+
+    const stateMaintainance = ( event ) => {  // Faith --> State maintainance
+        const metadataIdentifierElement = event.target.getAttribute('type');  // Return value corresponding, specific element attribute
+        
+        if( metadataIdentifierElement == 'email' ){ mutateIdentifier( event.target.value ) }
+        else{ mutateCredential( event.target.value ) }
+    }
+
     return (
         <React.Fragment>
             <div className="loginOutmostContainer">
                 <div className="loginChildrenContainer">
                     <div className="headlineContainer"><div className="headlineElement">LOGIN</div></div>
-                    <div className="metadataContainer">
+                    <div className="metadataContainer" onChange = { ( event ) => { stateMaintainance( event ) } }>
                         <div className="identifierContainer metadataChildrenContainer"><h1 className='metadataIdentifierElement'>Identifier</h1><input type="email" className="identifierElement metadataElement" placeholder="Provide identifer"/></div>
                         <div className="credentialContainer metadataChildrenContainer"><h1 className='metadataIdentifierElement'>Credential</h1><input type="password" className="credentialElement metadataElement" placeholder="Provide credential"/></div>
                     </div>
                     <div className="surrenderContainer"><button className='surrenderElement'>Surrender</button></div>
-                    <div className="redirectionContainer" onMouseEnter = { ( event ) => { console.log(event) } }>
+                    <div className="redirectionContainer">
                         <div className="redirectionChildrenContainer registrationContainer"><button className='registrationElement redirectionElement'>Registration</button></div>
                         <div className="redirectionChildrenContainer replaceContainer"><button className='replaceElement redirectionElement'>Replace credential</button></div>
                     </div>
