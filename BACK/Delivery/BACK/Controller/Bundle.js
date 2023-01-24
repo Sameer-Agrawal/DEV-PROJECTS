14a3260896e51642db7df074857c6447c2fef74a
Sameer-Agrawal <sameerag0812@outlook.com>
2023-01-25 00:22:31 +0530

    Datum mutation, provided bundle identifier

--- BACK/Delivery/BACK/Controller/Bundle.js
+++ BACK/Delivery/BACK/Controller/Bundle.js
@@ -39,18 +39,58 @@ const fabrication = async ( request , response ) => {  // Faith --> Bundle fabri
 
 const extraction = async ( request, response ) => {  // Faith --> Datum retrieval, provided bundle identifier
     try{
-        
         const identifier = request.params.identifier;  // Represent, bundle identifier
         const database_response = await bundle.findById( identifier );  // Datum retrieval, provided bundle identifier
 
         if( database_response ){  // Authenticate, bundle identifier
             response.json( { 'essence' : 'Successful datum retrieval, provided bundle identifier' , 'metadata' : database_response } )
         }
-
     }catch( blunder ){
         response.status( 500 ); 
         response.json( { essence : 'Blunder, bad gate-way' } );
     }
 }
 
-module.exports = { retrieval , fabrication , extraction }
+const mutation = async ( request , response ) => {  // Faith --> Datum mutation, provided bundle identifier
+    try{
+        const responsibility = request.metadata.responsibility;  // Represent, customer authority
+
+        if( responsibility == 'Administrator' || responsibility == 'Possessor' ){  // Access maintenance
+            const bundle_identifier = request.params.identifier;  // Represent bundle, unique identifier
+            const database_response = await bundle.findById( bundle_identifier );  // Datum retrieval, provided bundle identifier
+
+            if( database_response ){  // Authentic, bundle identifier
+                if( database_response.customer_identifier == request.identifier || responsibility == 'Administrator' ){  // Authentic customer holding up 'access' --> Mutate, bundle metadata
+                    const datum = request.body;  // Represent, datum
+
+                    if( Object.keys( datum ).length > 0 ){  // Represent, datum specified
+                        for ( const key in datum ) {  // Looping through, specified datum
+                            database_response[ key ] = datum[ key ];  // Mutation
+                        }
+    
+                        const translation = await database_response.save();  // 'Save', translated document
+    
+                        if( translation ){
+                            response.json( { essence : 'Successful, mutation' , metadata : translation } )
+                        }
+                    }else{
+                        response.status( 400 );  // Bad request, client blunder
+                        response.json( { 'essence' : 'Bad request, client blunder' } );
+                    }
+                }else{  // Corrupt customer
+                    response.status( 401 );  // Represent, corrupt credential
+                    response.json( { 'essence' : 'Access prohibited' } );
+                }
+            }
+
+        }else{
+            response.status( 401 );  // Represent, corrupt credential
+            response.json( { 'essence' : 'Access prohibited' } );
+        }
+    }catch( blunder ){
+        response.status( 502 );  // Bad gate-way
+        response.json( { 'essence' : blunder.message } );
+    }
+}
+
+module.exports = { retrieval , fabrication , extraction , mutation }
--- BACK/Delivery/BACK/ROUTER/Bundle.js
+++ BACK/Delivery/BACK/ROUTER/Bundle.js
@@ -3,11 +3,11 @@ const express = require('Express');
 // Mounting --> Gradually increasing
 const bundle = express.Router();  // Provide, object instance
 
-const { retrieval , fabrication , extraction } = require('../Controller/Bundle.js'); 
+const { retrieval , fabrication , extraction , mutation } = require('../Controller/Bundle.js'); 
 const { validation } = require('../Controller/Customer.js');
 
 bundle.route( '/' ).get( retrieval );  // Faith --> Return, collection
 bundle.route( '/fabrication' ).post( validation , fabrication );  // Faith --> Bundle document, fabrication
-bundle.route( '/:identifier' ).get( extraction )
+bundle.route( '/:identifier' ).get( extraction ).patch( validation , mutation );
 
 module.exports = { bundle }
